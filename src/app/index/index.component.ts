eb2b5382746d18237aecda4d3ead8312b3070a47
said benbouchta <saidbenbouchta93@gmail.com>
2024-01-12 10:56:22 +0100

    card

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component,CUSTOM_ELEMENTS_SCHEMA,OnInit } from '@angular/core';
 
 @Component({
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
@@ -8,23 +8,29 @@ import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class IndexComponent {
- speed1 :string="0";
- speed2:string="0";
+export class IndexComponent implements OnInit {
+  speed1 =0;
+  speed2=0;
+   ngOnInit() {
+    // Code to execute on component initialization
+   this.speed1=0;
+   this.speed2=0
+  }
+
  OnHovenring(){
-   this.speed1 = "0.5"
+   this.speed1 =0.5
    console.log(this.speed1)
 
  }
  OnMouseLeave(){
-  this.speed1 = "0"
+  this.speed1 = 0
   console.log(this.speed1)
 
 }
 OnHovenring2(){
-  this.speed2 = "0.5"
+  this.speed2 = 0.5
 }
 OnMouseLeave2(){
- this.speed2 = "0"
+ this.speed2 = 0
 }
 }
